f76a5d98d96a99fd5bf19c3b06d0b544ce01ce66
Magdalena.Horbaczyk <magdalena.horbaczyk@gmail.com>
2019-10-29 23:01:25 +0100

    sum_racing

---
+++ kodilla-prework/src/main/java/com/kodilla/sum_racing/Application.java
@@ -0,0 +1,19 @@
+package com.kodilla.sum_racing;
+
+import java.util.Random;
+
+public class Application {
+    public static void main(String[] args){
+        int counter = 0;
+        int sumaA = 1000;
+        int sumaB = 0;
+
+        Random randomGenerator = new Random();
+        while (sumaA>sumaB){
+            counter++;
+            System.out.println("Próba# " + "[" + counter + "] " + sumaA + " > " +  sumaB);
+            sumaA = sumaA + randomGenerator.nextInt(10);
+            sumaB = sumaB + randomGenerator.nextInt(50);
+        }
+    }
+}
