96da53f60a49b804017cd79bedfe980c780d21ae
Magdalena.Horbaczyk <magdalena.horbaczyk@gmail.com>
2019-10-29 12:08:35 +0100

    orders

---
+++ kodilla-prework/src/main/java/orders/Application.java
@@ -0,0 +1,19 @@
+package orders;
+
+import java.util.HashSet;
+
+public class Application {
+    public static void main (String[] args){
+        String orderNo = "zamówienie Nr";
+        Double value = 100.50;
+
+        HashSet<Order> ordersSet = new HashSet<Order>();
+        for(int n=0; n<100; n++){
+            ordersSet.add(new Order(orderNo+n, value+n));
+        }
+        ordersSet.add(new Order(orderNo+0, value+0));
+        System.out.println("Ilość zamówień: " + ordersSet.size());
+        System.out.println(ordersSet);
+
+    }
+}
---
+++ kodilla-prework/src/main/java/orders/Order.java
@@ -0,0 +1,35 @@
+package orders;
+
+public class Order {
+    private String orderNo;
+    private Double value;
+
+    public Order(String orderNo, Double value){
+        this.orderNo = orderNo;
+        this.value = value;
+    }
+
+    private String getOrderNo(){
+        return orderNo;
+    }
+
+    private Double getValue(){
+        return value;
+    }
+
+    @Override
+    public int hashCode(){
+        return orderNo.length();
+    }
+
+    @Override
+    public boolean equals(Object o){
+        Order e = (Order) o;
+        return (orderNo.equals(e.getOrderNo())) && (value.equals(e.getValue()));
+    }
+
+    @Override
+    public String toString(){
+        return orderNo + ", " + value + " PLN";
+    }
+}
