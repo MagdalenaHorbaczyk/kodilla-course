91285ac6e0429a19a4c2083c854f1408c03046b3
Magdalena <magdalena.horbaczyk@gmail.com>
2020-01-08 18:32:15 +0100

    refactor

--- kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
@@ -50,4 +50,16 @@ public class SpringRunnerTestSuite {
         //Then
         Assert.assertEquals("This is a square.", name);
     }
+
+    @Test
+    public void testShapeLoadedIntoContainer() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Shape shape = (Shape)context.getBean("chosenShape");
+        //When
+        String name = shape.getShapeName();
+        //Then
+        System.out.println("Chosen shape says: " + name);
+    }
 }
