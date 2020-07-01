0b12ee627c050f8483520548c6e56b93554b6356
Magdalena <magdalena.horbaczyk@gmail.com>
2020-07-01 17:41:31 +0200

    31.4 cleanUp_CrudApp_v3(accepting_allert_window_in_Selenium)

--- kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
+++ kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
@@ -66,6 +66,7 @@ public class CrudAppTestSuite {
                     buttonCreateCard.click();
                 });
         Thread.sleep(5000);
+        driver.switchTo().alert().accept();
     }
 
     private boolean checkTaskExistsInTrello(String taskName) throws InterruptedException {
